43d6b1ee5f1142bcd2033d4822efb7873a38cda4
doofmars <jan-preuss@gmx.de>
2022-02-06 23:10:43 +0100

    Parse html and write to JSON
    Soup the html table row to dict
    Check for null, nan and none values and act accordingly
    Write result to json

--- 1_fetch.py
+++ 1_fetch.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import json
 import os
 
 import requests as req
@@ -6,6 +7,7 @@ from bs4 import BeautifulSoup
 
 URL = "https://boardgamegeek.com/collection/user/Octavian?own=1"
 collection_file = os.path.join('cache', 'collection.html')
+collection_json = os.path.join('cache', 'collection.json')
 
 
 def get_data():
@@ -26,13 +28,44 @@ def get_data():
 
     soup = BeautifulSoup(html, 'html.parser')
     collection_table = soup.find(id='collectionitems')
+    collection = list()
+
+    # Iterate over collection table, store results to dict
     first = True
-    for collection_item in collection_table.find_all('tr'):
+    for collection_row in collection_table.find_all('tr'):
         if first:
             first = False
             continue
-        print(collection_item)
-        break
+        collection_item = dict()
+
+        collection_item['name'] = collection_row.find('a', class_='primary').text
+        version = collection_row.find('div', class_='geekitem_name')
+        if version is not None:
+            collection_item['version'] = version.text.strip()
+        collection_item['year'] = collection_row.find('span', class_='smallerfont').text[1:-1]
+        collection_item['id'] = collection_row.find('a', class_='primary')['href'].split('/')[2]
+        user_rating = collection_row.find('div', class_='ratingtext')
+        if user_rating is None:
+            collection_item['user_rating'] = None
+        else:
+            collection_item['user_rating'] = user_rating.text
+
+        geek_rating = collection_row.find('td', class_='collection_bggrating').text.strip()
+        if geek_rating == 'N/A':
+            collection_item['geek_rating'] = None
+        else:
+            collection_item['geek_rating'] = geek_rating
+        collection_item['status'] = collection_row.find('td', class_='collection_status').text.strip()
+        plays = collection_row.find('td', class_='collection_plays')
+        if plays.a is None:
+            collection_item['plays'] = 0
+        else:
+            collection_item['plays'] = int(plays.a.text)
+
+        collection.append(collection_item)
+
+    with open(collection_json, 'w', encoding='UTF-8') as fp:
+        json.dump(collection, fp, indent=2)
 
 
 if __name__ == '__main__':

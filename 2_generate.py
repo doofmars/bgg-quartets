3a4a49653e278bc4712f8db6431daa2a86195cc1
doofmars <jan-preuss@gmx.de>
2022-11-04 16:10:13 +0100

    Add an icon to the card
    Each meta value has a custom icon provided by
    iconmonstr or flaticon

--- 2_generate.py
+++ 2_generate.py
@@ -11,6 +11,20 @@ CONVERSION_IN_MM = 25.4
 
 config = configparser.ConfigParser()
 
+ICONS = [
+    ('iconmonstr-calendar-4-240.png', 9, 59),
+    ('iconmonstr-time-13-240.png', 9, 65),
+    ('iconmonstr-star-half-lined-240.png', 9, 71),
+    ('iconmonstr-product-14-240.png', 9, 77),
+    ('flaticon-weight.png', 9, 83),
+
+    ('iconmonstr-user-29-240.png', 33, 59),
+    ('iconmonstr-user-23-240.png', 33, 65),
+    ('flaticon-age-group.png', 33, 71),
+    ('iconmonstr-thumb-14-240.png', 33, 77),
+    ('iconmonstr-video-15-240.png', 33, 83),
+]
+
 
 def load_data():
     """
@@ -76,6 +90,13 @@ def fetch_image(id, url):
 
 
 def render_as_card(game, card_config, gen_config):
+    """
+    Render a game as a card
+
+    :param game: The game object to render
+    :param card_config: The configuration for the card
+    :param gen_config: The card generation configuration
+    """
     print_width = dpi(gen_config['WIDTH'])
     print_height = dpi(gen_config['HEIGHT'])
     cut_border = dpi(gen_config['CUT_BORDER'])
@@ -105,26 +126,41 @@ def render_as_card(game, card_config, gen_config):
     # draw multiline text
     d.text((dpi(9), dpi(52)), game.find('name').text, font=fnt, fill=(0, 0, 0))
 
-    d.text((dpi(10), dpi(58)), game.find('yearpublished').text, font=fnt, fill=(0, 0, 0))
-    d.text((dpi(10), dpi(64)), f"{game.find('stats').get('minplaytime')}-{game.find('stats').get('maxplaytime')}", font=fnt, fill=(0, 0, 0))
-    d.text((dpi(10), dpi(70)), game.find('./boardgame/statistics/ratings/average').text, font=fnt, fill=(0, 0, 0))
-    d.text((dpi(10), dpi(76)), game.find('./boardgame/statistics/ratings/owned').text, font=fnt, fill=(0, 0, 0))
-    d.text((dpi(10), dpi(82)), game.find('./boardgame/statistics/ratings/averageweight').text, font=fnt, fill=(0, 0, 0))
+    d.text((dpi(13), dpi(58)), game.find('yearpublished').text, font=fnt, fill=(0, 0, 0))
+    d.text((dpi(13), dpi(64)), f"{game.find('stats').get('minplaytime')}-{game.find('stats').get('maxplaytime')}", font=fnt, fill=(0, 0, 0))
+    d.text((dpi(13), dpi(70)), game.find('./boardgame/statistics/ratings/average').text, font=fnt, fill=(0, 0, 0))
+    d.text((dpi(13), dpi(76)), game.find('./boardgame/statistics/ratings/owned').text, font=fnt, fill=(0, 0, 0))
+    d.text((dpi(13), dpi(82)), game.find('./boardgame/statistics/ratings/averageweight').text, font=fnt, fill=(0, 0, 0))
 
-    d.text((dpi(35), dpi(58)), f"{game.find('stats').get('minplayers')}-{game.find('stats').get('maxplayers')}", font=fnt, fill=(0, 0, 0))
-    d.text((dpi(35), dpi(64)), game.find('./boardgame/age').text, font=fnt, fill=(0, 0, 0))
-    d.text((dpi(35), dpi(70)), game.find('./stats/rating').get('value'), font=fnt, fill=(0, 0, 0))
-    d.text((dpi(35), dpi(76)), game.find('numplays').text, font=fnt, fill=(0, 0, 0))
-    d.text((dpi(35), dpi(82)), game.find('yearpublished').text, font=fnt, fill=(0, 0, 0))
+    d.text((dpi(37), dpi(58)), f"{game.find('stats').get('minplayers')}-{game.find('stats').get('maxplayers')}", font=fnt, fill=(0, 0, 0))
+    d.text((dpi(37), dpi(64)), game.find('./boardgame/poll[@name="suggested_numplayers"]').get('totalvotes'), font=fnt, fill=(0, 0, 0))
+    d.text((dpi(37), dpi(70)), game.find('./boardgame/age').text, font=fnt, fill=(0, 0, 0))
+    d.text((dpi(37), dpi(76)), game.find('./stats/rating').get('value'), font=fnt, fill=(0, 0, 0))
+    d.text((dpi(37), dpi(82)), game.find('numplays').text, font=fnt, fill=(0, 0, 0))
     card_path = os.path.join(gen_config['CARD_CACHE'], f'{game_id}.png')
     # Add the game image
+    out.paste(game_image, (int(width / 2 - game_image.width / 2), dpi(14)))
 
-    # paste image centered
-    out.paste(game_image, (int(width/2 - game_image.width / 2), dpi(14)))
+    # load image with transparency
+    for icon in ICONS:
+        add_icon(out, icon[0], icon[1], icon[2])
 
     # Save results
     out.save(card_path)
     print(f'Created card for game {game_id} in {card_path}')
+
+
+def add_icon(out, icon_name, position_x, position_y):
+    """
+    Add an icon to the card
+
+    :param out: The image to add the icon to
+    :param icon_name: The name of the icon to add
+    :param position_x: The x position of the icon
+    :param position_y: The y position of the icon
+    """
+    icon = load_sized_image(os.path.join('resources', icon_name), dpi(3), dpi(3))
+    out.paste(icon, (dpi(position_x), dpi(position_y)), icon)
 
 
 def load_sized_image(image_path, max_width, max_height):
@@ -138,7 +174,7 @@ def load_sized_image(image_path, max_width, max_height):
     img = Image.open(image_path)
     width, height = img.size
     ratio = min(max_width / width, max_height / height)
-    return img.resize((int(width * ratio),  int(height * ratio)))
+    return img.resize((int(width * ratio), int(height * ratio)))
 
 
 def dpi(length) -> int:
--- README.md
+++ README.md
@@ -8,3 +8,21 @@ Setup, create venv, install requirements and start the scripts
     source venv/Scripts/activate
     pip install -r requirements.txt
     python 1_fetch.py
+
+# Icons
+
+Icons can be found on http://iconmonstr.com and www.flaticon.com/,
+the Icons are not included as per licence agreement.
+
+Icons used:
+
+ - <a href="https://www.flaticon.com/free-icons/age-group" title="age group icons">Age group icons created by Freepik - Flaticon</a>
+ - <a href="https://www.flaticon.com/free-icons/weight" title="weight icons">Weight icons created by Freepik - Flaticon</a>
+ - https://iconmonstr.com/calendar-4-png
+ - https://iconmonstr.com/product-14-png
+ - https://iconmonstr.com/star-half-lined-png
+ - https://iconmonstr.com/thumb-14-png
+ - https://iconmonstr.com/time-13-png
+ - https://iconmonstr.com/user-23-png
+ - https://iconmonstr.com/user-29-png
+ - https://iconmonstr.com/video-15-png
